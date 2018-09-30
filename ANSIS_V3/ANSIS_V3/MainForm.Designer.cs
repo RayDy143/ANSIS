63ebd71971f92fa2c7234921c0c4dfea9ab47fab
RayDy143 <alfeche492@outlook.com>
2018-09-30 17:53:15 +0800

    Initial Commit

---
+++ ANSIS_V3/ANSIS_V3/MainForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace ANSIS_V3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.UserAccountTile = new MetroFramework.Controls.MetroTile();
+			this.metroTile2 = new MetroFramework.Controls.MetroTile();
+			this.metroTile3 = new MetroFramework.Controls.MetroTile();
+			this.metroTile4 = new MetroFramework.Controls.MetroTile();
+			this.metroTile5 = new MetroFramework.Controls.MetroTile();
+			this.metroTile6 = new MetroFramework.Controls.MetroTile();
+			this.metroTile7 = new MetroFramework.Controls.MetroTile();
+			this.metroTile8 = new MetroFramework.Controls.MetroTile();
+			this.metroTile9 = new MetroFramework.Controls.MetroTile();
+			this.metroTile10 = new MetroFramework.Controls.MetroTile();
+			this.SuspendLayout();
+			// 
+			// UserAccountTile
+			// 
+			this.UserAccountTile.ActiveControl = null;
+			this.UserAccountTile.Location = new System.Drawing.Point(77, 164);
+			this.UserAccountTile.Name = "UserAccountTile";
+			this.UserAccountTile.Size = new System.Drawing.Size(225, 190);
+			this.UserAccountTile.TabIndex = 0;
+			this.UserAccountTile.Text = "User Accounts";
+			this.UserAccountTile.UseSelectable = true;
+			this.UserAccountTile.Click += new System.EventHandler(this.UserAccountTile_Click);
+			// 
+			// metroTile2
+			// 
+			this.metroTile2.ActiveControl = null;
+			this.metroTile2.Location = new System.Drawing.Point(321, 164);
+			this.metroTile2.Name = "metroTile2";
+			this.metroTile2.Size = new System.Drawing.Size(225, 190);
+			this.metroTile2.TabIndex = 1;
+			this.metroTile2.Text = "Student Profile";
+			this.metroTile2.UseSelectable = true;
+			// 
+			// metroTile3
+			// 
+			this.metroTile3.ActiveControl = null;
+			this.metroTile3.Location = new System.Drawing.Point(562, 164);
+			this.metroTile3.Name = "metroTile3";
+			this.metroTile3.Size = new System.Drawing.Size(225, 190);
+			this.metroTile3.TabIndex = 2;
+			this.metroTile3.Text = "Teacher Information";
+			this.metroTile3.UseSelectable = true;
+			// 
+			// metroTile4
+			// 
+			this.metroTile4.ActiveControl = null;
+			this.metroTile4.Location = new System.Drawing.Point(803, 164);
+			this.metroTile4.Name = "metroTile4";
+			this.metroTile4.Size = new System.Drawing.Size(225, 190);
+			this.metroTile4.TabIndex = 3;
+			this.metroTile4.Text = "Section";
+			this.metroTile4.UseSelectable = true;
+			// 
+			// metroTile5
+			// 
+			this.metroTile5.ActiveControl = null;
+			this.metroTile5.Location = new System.Drawing.Point(1059, 164);
+			this.metroTile5.Name = "metroTile5";
+			this.metroTile5.Size = new System.Drawing.Size(225, 190);
+			this.metroTile5.TabIndex = 4;
+			this.metroTile5.Text = "Books";
+			this.metroTile5.UseSelectable = true;
+			// 
+			// metroTile6
+			// 
+			this.metroTile6.ActiveControl = null;
+			this.metroTile6.Location = new System.Drawing.Point(1059, 373);
+			this.metroTile6.Name = "metroTile6";
+			this.metroTile6.Size = new System.Drawing.Size(225, 190);
+			this.metroTile6.TabIndex = 9;
+			this.metroTile6.Text = "Inquiry";
+			this.metroTile6.UseSelectable = true;
+			// 
+			// metroTile7
+			// 
+			this.metroTile7.ActiveControl = null;
+			this.metroTile7.Location = new System.Drawing.Point(803, 373);
+			this.metroTile7.Name = "metroTile7";
+			this.metroTile7.Size = new System.Drawing.Size(225, 190);
+			this.metroTile7.TabIndex = 8;
+			this.metroTile7.Text = "Reports";
+			this.metroTile7.UseSelectable = true;
+			// 
+			// metroTile8
+			// 
+			this.metroTile8.ActiveControl = null;
+			this.metroTile8.Location = new System.Drawing.Point(562, 373);
+			this.metroTile8.Name = "metroTile8";
+			this.metroTile8.Size = new System.Drawing.Size(225, 190);
+			this.metroTile8.TabIndex = 7;
+			this.metroTile8.Text = "Inventory";
+			this.metroTile8.UseSelectable = true;
+			// 
+			// metroTile9
+			// 
+			this.metroTile9.ActiveControl = null;
+			this.metroTile9.Location = new System.Drawing.Point(321, 373);
+			this.metroTile9.Name = "metroTile9";
+			this.metroTile9.Size = new System.Drawing.Size(225, 190);
+			this.metroTile9.TabIndex = 6;
+			this.metroTile9.Text = "Monitoring";
+			this.metroTile9.UseSelectable = true;
+			// 
+			// metroTile10
+			// 
+			this.metroTile10.ActiveControl = null;
+			this.metroTile10.Location = new System.Drawing.Point(77, 373);
+			this.metroTile10.Name = "metroTile10";
+			this.metroTile10.Size = new System.Drawing.Size(225, 190);
+			this.metroTile10.TabIndex = 5;
+			this.metroTile10.Text = "Transaction";
+			this.metroTile10.UseSelectable = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1350, 729);
+			this.Controls.Add(this.metroTile6);
+			this.Controls.Add(this.metroTile7);
+			this.Controls.Add(this.metroTile8);
+			this.Controls.Add(this.metroTile9);
+			this.Controls.Add(this.metroTile10);
+			this.Controls.Add(this.metroTile5);
+			this.Controls.Add(this.metroTile4);
+			this.Controls.Add(this.metroTile3);
+			this.Controls.Add(this.metroTile2);
+			this.Controls.Add(this.UserAccountTile);
+			this.ForeColor = System.Drawing.Color.Black;
+			this.Name = "MainForm";
+			this.Text = "Student Information System of Abellana National High School";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.ResumeLayout(false);
+
+        }
+
+		#endregion
+
+		private MetroFramework.Controls.MetroTile UserAccountTile;
+		private MetroFramework.Controls.MetroTile metroTile2;
+		private MetroFramework.Controls.MetroTile metroTile3;
+		private MetroFramework.Controls.MetroTile metroTile4;
+		private MetroFramework.Controls.MetroTile metroTile5;
+		private MetroFramework.Controls.MetroTile metroTile6;
+		private MetroFramework.Controls.MetroTile metroTile7;
+		private MetroFramework.Controls.MetroTile metroTile8;
+		private MetroFramework.Controls.MetroTile metroTile9;
+		private MetroFramework.Controls.MetroTile metroTile10;
+	}
+}
